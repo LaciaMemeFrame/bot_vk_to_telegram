4e5b499c17103106ed3a06d490ffd4a0ad7aeda4
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-08-22 00:41:59 +0300

    Удаление лишнего и небольшая оптимизация

--- vk_to_telegramm.py
+++ vk_to_telegramm.py
@@ -20,12 +20,6 @@ COUNT = config.get('VK', 'COUNT')
 VK_TOKEN = config.get('VK', 'TOKEN', fallback=None)
 BOT_TOKEN = config.get('Telegram', 'BOT_TOKEN')
 CHANNEL = config.get('Telegram', 'CHANNEL')
-INCLUDE_LINK = config.getboolean('Settings', 'INCLUDE_LINK')
-PREVIEW_LINK = config.getboolean('Settings', 'PREVIEW_LINK')
-
-# Символы, на которых можно разбить сообщение
-message_breakers = [':', ' ', '\n']
-max_message_length = 4091
 
 # Инициализируем телеграмм бота
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -66,7 +60,6 @@ def get_data(domain_vk, count_vk):
 def check_posts_vk():
     global DOMAIN
     global COUNT
-    global INCLUDE_LINK
     global bot
     global config
     global config_path
@@ -86,8 +79,6 @@ def check_posts_vk():
         print('------------------------------------------------------------------------------------------------')
         print(post)
 
-        # Текст
-        text = post['text']
 
         # Проверяем есть ли что то прикрепленное к посту
         images = []
@@ -111,50 +102,12 @@ def check_posts_vk():
                         if key != 'type' and 'url' in value:
                             attachments.append(value['url'])
 
-        if INCLUDE_LINK:
-            post_url = "https://vk.com/" + DOMAIN + "?w=wall" + \
-                str(post['owner_id']) + '_' + str(post['id'])
-            links.insert(0, post_url)
-        text = '\n'.join([text] + links)
-        send_posts_text(text)
-
         if len(images) > 0:
             image_urls = list(map(lambda img: max(
                 img["sizes"], key=lambda size: size["type"])["url"], images))
             print(image_urls)
             bot.send_media_group(CHANNEL, map(
                 lambda url: InputMediaPhoto(url), image_urls))
-
-        # Проверяем есть ли репост другой записи
-        if 'copy_history' in post:
-            copy_history = post['copy_history']
-            copy_history = copy_history[0]
-            print('--copy_history--')
-            print(copy_history)
-            text = copy_history['text']
-            send_posts_text(text)
-
-            # Проверяем есть ли у репоста прикрепленное сообщение
-            if 'attachments' in copy_history:
-                copy_add = copy_history['attachments']
-                copy_add = copy_add[0]
-
-                # Если это ссылка
-                if copy_add['type'] == 'link':
-                    link = copy_add['link']
-                    text = link['title']
-                    send_posts_text(text)
-                    img = link['photo']
-                    send_posts_img(img)
-                    url = link['url']
-                    send_posts_text(url)
-
-                # Если это картинки
-                if copy_add['type'] == 'photo':
-                    attach = copy_history['attachments']
-                    for img in attach:
-                        image = img['photo']
-                        send_posts_img(image)
 
         # Записываем id в файл
         config.set('Settings', 'LAST_ID', str(post['id']))
@@ -163,36 +116,6 @@ def check_posts_vk():
 
 
 # Отправляем посты в телеграмм
-
-
-# Текст
-def send_posts_text(text):
-    global CHANNEL
-    global PREVIEW_LINK
-    global bot
-
-    if text == '':
-        print('no text')
-    else:
-        # В телеграмме есть ограничения на длину одного сообщения в 4091 символ, разбиваем длинные сообщения на части
-        for msg in split(text):
-            bot.send_message(CHANNEL, msg, disable_web_page_preview=not PREVIEW_LINK)
-
-
-def split(text):
-    global message_breakers
-    global max_message_length
-
-    if len(text) >= max_message_length:
-        last_index = max(
-            map(lambda separator: text.rfind(separator, 0, max_message_length), message_breakers))
-        good_part = text[:last_index]
-        bad_part = text[last_index + 1:]
-        return [good_part] + split(bad_part)
-    else:
-        return [text]
-
-
 # Изображения
 def send_posts_img(img):
     global bot
@@ -206,4 +129,4 @@ if __name__ == '__main__':
     check_posts_vk()
     while True:
         check_posts_vk()
-        sleep(30)
+        sleep(3600)
